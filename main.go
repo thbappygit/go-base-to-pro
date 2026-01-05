bfe325950e313aec2ac8d868dd7fc6bed098b6bc
tanvir.bappy <tanvir.bappy@bansberrysg.com>
2026-01-05 23:50:19 +0600

    defer

--- main.go
+++ main.go
@@ -2,20 +2,44 @@ package main
 
 import "fmt"
 
+func sayMessage() (message string) {
+	message = "Hello World"
+	fmt.Println(message)
+
+	showNewMessage := func() {
+		message = "Hello Golang"
+		fmt.Println("defer", message)
+	}
+
+	defer showNewMessage()
+
+	message = "Hello Universe"
+	fmt.Println("last", message)
+	return
+
+}
+func expenses() int {
+	ex := 100
+	ex2 := 200
+	newExpense := func() {
+		ex = ex + ex2
+	}
+	defer newExpense()
+	ex = 50
+
+	return ex
+
+}
+
 func main() {
-	var testData []int
 
-	testData = make([]int, 5)
-
-	fmt.Println(testData, cap(testData))
-
-	testData[0] = 5
-	testData = append(testData, 10, 20)
-	testData = append(testData, 30, 40, 50)
-	newTestData := testData[4:6]
-
-	fmt.Println(testData, newTestData)
-
-	//myData := testData[:2]
+	sayMessage()
+	fmt.Println(expenses())
+	var data = [5]int{1, 2, 3, 4, 5}
+	for _, v := range data {
+		func() {
+			defer fmt.Println(v)
+		}()
+	}
 
 }
