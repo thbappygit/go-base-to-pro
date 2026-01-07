6e4bd2df5dc4304c1e16da0141ab16d74c018341
tanvir.bappy <tanvir.bappy@bansberrysg.com>
2026-01-07 16:32:41 +0600

    --

--- main.go
+++ main.go
@@ -2,32 +2,42 @@ package main
 
 import "fmt"
 
-var arrOfColleagues = [5]string{"Bappy", "Emon", "Miraz", "Nasim", "Forhad"}
-var justice = [2]bool{true, false}
+func testProg() (hello string) {
+	hello = "Hello World"
+	fmt.Println(hello)
+	defer func() {
+		hello = "Hello For Closer defer"
+		fmt.Println(hello)
+	}()
 
-func init() {
-	fmt.Println("Welcome to array learning!")
-}
+	defer func() {
+		hello = "Hello   defer2"
+		fmt.Println(hello)
+	}()
 
-func main() {
-	fmt.Println(justice)
-
-	var arrNumbers [5]int
-	fmt.Println(arrOfColleagues[1], "and", arrOfColleagues[0], "are in same team")
-
-	fmt.Println(arrNumbers[0], arrNumbers[4])
-
-	length := len(arrNumbers)
-	fmt.Println(length)
-
-	b := [5]int{1, 2, 3, 4, 5}
-	fmt.Println("dcl:", b)
-
-	b = [...]int{7, 8, 9, 10, 11}
-	fmt.Println("dcl2:", b)
-
-	for index, value := range b {
-		fmt.Println(index, value)
+	var arrData = []int{1, 2, 3, 4, 5}
+	for _, value := range arrData {
+		func() {
+			defer fmt.Println("I love you", value)
+		}()
 	}
+	arrData = append(arrData, 6, 7, 8)
+	arrData = make([]int, 10, 11)
 
+	arrData[2] = 3
+	arrData = arrData[:3]
+	fmt.Println(arrData)
+
+	fmt.Println("sliced", arrData)
+
+	fmt.Println("Array Length:", len(arrData))
+	fmt.Println("Array Capacity:", cap(arrData))
+
+	hello = "Hello after Closer defer"
+	fmt.Println(hello)
+	return
+
+}
+func main() {
+	testProg()
 }
