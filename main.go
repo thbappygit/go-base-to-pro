c0de644742318ce87721ebcae428afe3ab83e851
thbappy7706 <tanvir15-7706@diu.edu.bd>
2026-01-17 23:57:28 +0600

    go routine

--- main.go
+++ main.go
@@ -1,43 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-func testProg() (hello string) {
-	hello = "Hello World"
-	fmt.Println(hello)
-	defer func() {
-		hello = "Hello For Closer defer"
-		fmt.Println(hello)
-	}()
-
-	defer func() {
-		hello = "Hello   defer2"
-		fmt.Println(hello)
-	}()
-
-	var arrData = []int{1, 2, 3, 4, 5}
-	for _, value := range arrData {
-		func() {
-			defer fmt.Println("I love you", value)
-		}()
-	}
-	arrData = append(arrData, 6, 7, 8)
-	arrData = make([]int, 10, 11)
-
-	arrData[2] = 3
-	arrData = arrData[:3]
-	fmt.Println(arrData)
-
-	fmt.Println("sliced", arrData)
-
-	fmt.Println("Array Length:", len(arrData))
-	fmt.Println("Array Capacity:", cap(arrData))
-
-	hello = "Hello after Closer defer"
-	fmt.Println(hello)
-	return
-
+func addCalc(a, b int) {
+	fmt.Println(a + b)
 }
+
+func printHello(num int) {
+	fmt.Println("Hello Bappy", num)
+	addCalc(10, 20)
+}
+
 func main() {
-	testProg()
+	go printHello(1)
+	go printHello(2)
+	go printHello(3)
+	go printHello(4)
+	go printHello(5)
+
+	time.Sleep(4 * time.Second)
 }
